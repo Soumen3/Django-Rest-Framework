ab3d42c7db9022efbe4d33bd36e79aacbb030dae
Soumen3 <soumensamanta112233@gmail.com>
2024-04-06 20:46:47 +0530

    token generation using admin application, manage.py command, api endpoint

---
+++ TokenAuthentication/api/views.py
@@ -0,0 +1,15 @@
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import DjangoModelPermissionsOrAnonReadOnly
+# Creating ModelViewSet
+class StudentModelViewSet(viewsets.ModelViewSet):
+	queryset = Student.objects.all()
+	serializer_class = StudentSerializer
+
+	# authentication_classes = [SessionAuthentication]
+	# permission_classes=[DjangoModelPermissionsOrAnonReadOnly]
+ 
+	
