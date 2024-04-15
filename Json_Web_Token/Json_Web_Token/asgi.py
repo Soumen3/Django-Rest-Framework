11e972d615c232744ad47f3199b3672fff3dfef4
Soumen3 <soumensamanta112233@gmail.com>
2024-04-15 12:17:12 +0530

    json web token

---
+++ Json_Web_Token/Json_Web_Token/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Json_Web_Token project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Json_Web_Token.settings')
+
+application = get_asgi_application()
