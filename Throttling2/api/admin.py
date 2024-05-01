639910389e852723e0357a4ab6e8c613455f707d
Soumen3 <soumensamanta112233@gmail.com>
2024-05-01 14:58:24 +0530

    Throttling

---
+++ Throttling2/api/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Student
+
+# Register your models here.
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+	list_display = ['id', 'name', 'roll', 'city']
+	list_display_links = ['name']
+	search_fields = ['name', 'city']
+	list_filter = ['city']
+	list_editable = ['roll']
